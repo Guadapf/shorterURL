5fe3e094aed356bcbec9ff5951230ecf523035d7
Fernandez Guadalupe <guadapfernandez@gmail.com>
2025-02-24 15:57:58 -0300

    add dto UrlRequest

---
+++ src/main/java/com/su/shorterUrl/model/dto/UrlRequest.java
@@ -0,0 +1,13 @@
+package com.su.shorterUrl.model.dto;
+
+public class UrlRequest {
+    private String url;
+
+    public void setUrl(String url){
+        this.url = url;
+    }
+
+    public String getUrl(){
+        return this.url;
+    }
+}
